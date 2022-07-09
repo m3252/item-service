fac8e6405c1abcf5e14ff56cae1617cf33c01798
noah <m3252@naver.com>
2022-07-09 23:58:19 +0900

    feat : 상품 수정, 등록 페이지

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -25,7 +26,17 @@ public class BasicItemController {
         return "basic/items";
     }
 
-//    @GetMapping("add")
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return "addForm";
+    }
 
     /**
      * 테스트 아이템 추가
